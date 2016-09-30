88d215f54821b0f8b4c156bf1294fc7824d495ea
Adrienn Szabo <aszabo@ilab.sztaki.hu>
2016-09-30 14:52:40 +0200

    commentlist and form skeletons added

--- public/scripts/mytutorial.js
+++ public/scripts/mytutorial.js
@@ -6,6 +6,29 @@ var CommentBox = React.createClass({
     return (
       <div className="myCommentBox">
         Hello, world! I am a CommentBox.
+        <h1>Comments</h1>
+        <CommentList />
+        <CommentForm />
+        </div>
+    );
+  }
+});
+
+var CommentList = React.createClass({
+  render: function() {
+    return (
+      <div className="commentList">
+        Hello, world! I am a CommentList.
+      </div>
+    );
+  }
+});
+
+var CommentForm = React.createClass({
+  render: function() {
+    return (
+      <div className="commentForm">
+        Hello, world! I am a CommentForm.
       </div>
     );
   }
@@ -15,3 +38,4 @@ ReactDOM.render(
   <CommentBox />,
   document.getElementById('content')
 );
+
