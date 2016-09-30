6f00983970ba9a7c77709bea15ee471d07169ff8
Adrienn Szabo <aszabo@ilab.sztaki.hu>
2016-09-30 15:03:02 +0200

    commentlist shows comments from script

--- public/scripts/mytutorial.js
+++ public/scripts/mytutorial.js
@@ -18,7 +18,10 @@ var CommentList = React.createClass({
   render: function() {
     return (
       <div className="commentList">
-        Hello, world! I am a CommentList.
+        <b>Hello, world! I am a CommentList.</b>
+        <Comment author="Pete Hunt">This is one comment</Comment>
+        <Comment author="Jordan Walke">This is *another* comment</Comment>
+        <b>This is the end of CommentList.</b>
       </div>
     );
   }
@@ -29,6 +32,19 @@ var CommentForm = React.createClass({
     return (
       <div className="commentForm">
         Hello, world! I am a CommentForm.
+      </div>
+    );
+  }
+});
+
+var Comment = React.createClass({
+  render: function() {
+    return (
+      <div className="comment">
+        <h2 className="commentAuthor">
+          {this.props.author}
+        </h2>
+        {this.props.children}
       </div>
     );
   }
