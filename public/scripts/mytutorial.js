9d106ef7bba09d312db8ce4a2a08615fe6e1c02f
Adrienn Szabo <aszabo@ilab.sztaki.hu>
2016-09-30 15:09:14 +0200

    commentlist shows comments in markdown (at step 7 of tutorial)

--- public/scripts/mytutorial.js
+++ public/scripts/mytutorial.js
@@ -38,13 +38,19 @@ var CommentForm = React.createClass({
 });
 
 var Comment = React.createClass({
+  rawMarkup: function() {
+    var md = new Remarkable();
+    var rawMarkup = md.render(this.props.children.toString());
+    return { __html: rawMarkup };
+  },
   render: function() {
+    var md = new Remarkable();
     return (
       <div className="comment">
         <h2 className="commentAuthor">
           {this.props.author}
         </h2>
-        {this.props.children}
+        <span dangerouslySetInnerHTML={this.rawMarkup()} />
       </div>
     );
   }
